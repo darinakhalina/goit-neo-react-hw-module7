c1d187c2d47021fe11e9ebf734c6e4c170eed9dd
Darina Khalina <darina.khalina@namecheap.com>
2024-10-19 18:35:40 +0300

    chore: remove redux-persist

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Demo Link -
+Demo Link - https://goit-neo-react-hw-module7-git-main-darina-khalinas-projects.vercel.app/
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,10 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist/es/constants';
-import { thunk } from 'redux-thunk';
-import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
-const persistContactsConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const rootReducer = combineReducers({
-  contacts: persistReducer(persistContactsConfig, contactsReducer),
-  filters: filtersReducer,
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(thunk),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
 });
-
-export const persistor = persistStore(store);
